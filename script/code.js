e435c6a0a7cbfcb22835b426e3629bf535a72616
Nusaibah01 <charifnusaibah01@gmail.com>
2024-05-23 09:21:21 +0200

    js

---
+++ script/code.js
@@ -0,0 +1,14 @@
+// Greet Function 
+function greet() {
+    console.log(`Hello`);
+}
+// Product Function
+function multiply(num1, num2 ) {
+   console.log(`num1 * numb2`)
+multiply(numb1, numb2);
+}
+
+// tableOfFive Function
+function tableOfFive() {
+  
+}
